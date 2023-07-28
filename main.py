fc1f8d10097af86a03dc029328c7a532d5056ef0
Karnpapon Boonput <karnpapon@gmail.com>
2023-07-28 23:33:51 +0700

    add new ghost-board / refactoring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-import math
 import os
 from pygame.locals import *
 from easing_functions import *
@@ -16,23 +15,20 @@ load_dotenv()
 # import pyttsx3
 
 pygame.init()
-screen = pygame.display.set_mode((600, 500))
 quit_app = False
 commands = queue.Queue()
 
 # Display
-WIDTH = 1280
-HEIGHT = 720
+WIDTH = 835
+HEIGHT = 470
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PTK By @Karnpapon")
-bg = pygame.image.load(os.path.join("Images", "ouija-board.jpeg"))
+bg = pygame.image.load(os.path.join("Images", "ghost-board.png"))
 bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
 
 openai.api_key = os.getenv("OPENAI_API")
-user = "Person:"
-ai = "Geist:"
-prompt_text = "GHOST"
-
+USER = "Person:"
+PROMPT_TEXT = "GHOST"
 
 # accel
 MAX_SPEED = 20
@@ -43,58 +39,55 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 PURPLE = (142, 68, 173)
-INIT_POINT_X = 105
-INIT_POINT_Y = 105
-FPS = 30
+INIT_POINT_X = 400
+INIT_POINT_Y = 80
+FPS = 60
 DECELERATION = 5
 
-CHARACTERS = {'A': (142, 386),
-              'Ä': (142, 386),
-              ' ': (1280/2, (720/2) + 120),
-              'B': (230, 339),
-              'C': (313, 310),
-              'D': (394, 285),
-              'E': (475, 269),
-              'F': (558, 260),
-              'G': (630, 257),
-              'H': (721, 258),
-              'I': (795, 269),
-              'J': (855, 277),
-              'K': (941, 304),
-              'L': (1031, 334),
-              'M': (1112, 371),
-              'N': (163, 545),
-              'O': (231, 495),
-              'Ö': (231, 495),
-              'P': (303, 452),
-              'Q': (376, 423),
-              'R': (460, 401),
-              'S': (543, 382),
-              'T': (621, 377),
-              'U': (706, 379),
-              'Ü': (706, 379),
-              'V': (792, 384),
-              'W': (884, 416),
-              'X': (977, 453),
-              'Y': (1050, 491),
-              'Z': (1120, 539),
-              '1': (291, 611),
-              '2': (350, 614),
-              '3': (428, 614),
-              '4': (506, 614),
-              '5': (588, 616),
-              '6': (668, 614),
-              '7': (747, 615),
-              '8': (824, 616),
-              '9': (905, 615),
-              '0': (985, 610)}
+CHARACTERS = {'A': (100, 212),
+              ' ': (626, 354),
+              'B': (150, 208),
+              'C': (206, 208),
+              'D': (251, 208),
+              'E': (302, 208),
+              'F': (342, 208),
+              'G': (398, 208),
+              'H': (441, 208),
+              'I': (485, 208),
+              'J': (524, 208),
+              'K': (568, 208),
+              'L': (611, 208),
+              'M': (662, 218),
+              'N': (104, 283),
+              'O': (161, 283),
+              'P': (216, 283),
+              'Q': (271, 283),
+              'R': (324, 283),
+              'S': (378, 283),
+              'T': (419, 283),
+              'U': (455, 283),
+              'V': (503, 283),
+              'W': (554, 283),
+              'X': (655, 283),
+              'Y': (705, 283),
+              'Z': (755, 283),
+              '0': (99, 344),
+              '1': (145, 344),
+              '2': (200, 344),
+              '3': (245, 344),
+              '4': (297, 344),
+              '5': (342, 344),
+              '6': (396, 344),
+              '7': (442, 344),
+              '8': (491, 344),
+              '9': (544, 344)}
 
 
-class Ball(object):
+class Pointer(object):
     def __init__(self, x, y, color):
         self.color = color
         self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(2, 2)
+        self.velocity = pygame.Vector2(4, 4)
         self.acceleration = pygame.Vector2(0.5, 0.5)
         self.friction = 0.95
 
@@ -119,11 +112,11 @@ class Ball(object):
 
 # Game Variables
 clock = pygame.time.Clock()
-ball = Ball(INIT_POINT_X, INIT_POINT_Y, RED)
+pointer = Pointer(INIT_POINT_X, INIT_POINT_Y, RED)
 
 
 def ask(question):
-    text = f"{prompt_text}\n{user} {question}"
+    text = f"{PROMPT_TEXT}\n{USER} {question}"
     response = openai.Completion.create(
         model="text-davinci-002",
         prompt=text,
@@ -154,20 +147,18 @@ def main():
         if not command == None:
             answer = command
             answer = answer.upper()
-            print("answer", answer, answer_index)
             to = pygame.Vector2(
                 CHARACTERS[answer[answer_index]][0], CHARACTERS[answer[answer_index]][1])
 
-        screen.blit(bg, (0, 0))
+        WINDOW.blit(bg, (0, 0))
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
         if answer_index <= len(answer) and answer:
-            if ball.position.distance_to(to) < 15 + 15:
+            if pointer.position.distance_to(to) < 15 + 15:
                 timeout -= 1
-                # print("timeout:", timeout)
 
         if timeout == 0:
             if to == pygame.math.Vector2(INIT_POINT_X, INIT_POINT_Y):
@@ -193,15 +184,15 @@ def main():
             timeout = FPS * 4
 
         ghost_msg = pygame.font.SysFont("Arial", 50, italic=True)
-        ghost_msg = ghost_msg.render(str(current_answer), True, WHITE)
+        ghost_msg = ghost_msg.render(str(current_answer), True, RED)
         WINDOW.blit(ghost_msg, (150, 0))
 
         if (answer):
-            ball.Move(to)
+            pointer.Move(to)
 
-        ball.Draw(WINDOW)
+        pointer.Draw(WINDOW)
         client.send_message(
-            "/synth_coord", [ball.position.x / WIDTH, 1.0 - ball.position.y / HEIGHT])
+            "/synth_coord", [pointer.position.x / WIDTH, 1.0 - pointer.position.y / HEIGHT])
 
         pygame.display.update()
         clock.tick(FPS)
@@ -211,9 +202,9 @@ class Input(threading.Thread):
     def run(self):
         while not quit_app:
             question = input()
-            question = user + question.lower() + '\n'
+            question = USER + question.lower() + '\n'
             # answer = ask(question)
-            mockup_ans = random.choice(["ab", "bc", "cd"])
+            mockup_ans = random.choice(["I'm a ghost", "oxygen", "zebra"])
             commands.put(mockup_ans)
             # print("commands: ", commands.get())
 
